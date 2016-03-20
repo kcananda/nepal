84e5c917f45b6a21e6b1e79ffce702f87fbd6dc9
ananda kc <kcananda@anandas-MBP.lan>
2016-03-20 12:35:16 +0100

    this is commit to check the revert funtion

--- multiplynumber.py
+++ multiplynumber.py
@@ -5,7 +5,9 @@ try:
 	a=float(aa)
 	bb = raw_input("enter the 2nd number")
 	b=float(bb)
+	git
 	print a*b
+	print "writing this one for the revert of the commit"
 
 except:
 	print "Enter the valid number"
