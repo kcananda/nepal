93d1c2a1d687eb176915575858f8f6f75de35894
ananda kc <kcananda@anandas-MBP.lan>
2016-03-20 11:53:52 +0100

    multiply program is added

---
+++ multiplynumber.py
@@ -0,0 +1,11 @@
+# program to add two mulitply two number
+
+try:
+	aa = raw_input("enter the 1st number")
+	a=float(aa)
+	bb = raw_input("enter the 2nd number")
+	b=float(bb)
+	print a*b
+
+except:
+	print "Enter the valid number"
